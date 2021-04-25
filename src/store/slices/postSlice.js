c4236539d548f82085218fa8c449cec646101ede
nikita.golovchenko <nikita.golovchenko@p2h.com>
2021-04-26 01:23:46 +0300

    fix

--- src/store/slices/postSlice.js
+++ src/store/slices/postSlice.js
@@ -12,7 +12,8 @@ export const getPost = createAsyncThunk('post/getPost', async (id) => {
   const posts = JSON.parse(localStorage.getItem('posts'));
 
   if (posts[id]) {
-    return {...response.data, comments: posts[id]};
+    console.log({...response.data, comments: posts[id]});
+    // return {...response.data, comments: posts[id]};
   }
 
   return response.data;
